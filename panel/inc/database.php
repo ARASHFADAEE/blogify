7f87ea8aba43a70487782cbdac85a6ff6b6aa936
arashfadaee <hosseinfdnews@gmail.com>
2025-04-20 08:58:30 +0330

    add show user page in panel

--- panel/inc/database.php
+++ panel/inc/database.php
@@ -8,10 +8,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Connected successfully";
   } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
+    // echo "Connection failed: " . $e->getMessage();
+  };
   
-
-
-
 ?>
