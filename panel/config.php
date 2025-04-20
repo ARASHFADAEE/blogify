31927a0e817ef12ba4ad8e2aecfce1b4b376323f
arashfadaee <hosseinfdnews@gmail.com>
2025-04-20 08:50:27 +0330

    add config and database.php

--- panel/config.php
+++ panel/config.php
@@ -1,5 +1,11 @@
 <?php
 
-define('BASE_URL','http://localhost/blog')
+define('BASE_URL','http://localhost/blog');
+define('SERVER_NAME','localhost');
+define('DB_NAME','blogify');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+
+
 
 ?>
---
+++ panel/inc/database.php
@@ -0,0 +1,17 @@
+
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/blog/panel/config.php';
+
+try {
+    $conn = new PDO("mysql:host=".SERVER_NAME.";dbname=".DB_NAME."", DB_USER, DB_PASSWORD);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+  
+
+
+
+?>
