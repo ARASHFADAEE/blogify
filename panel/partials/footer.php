84860067d463a1505100a824feea03b402d34d7b
arashfadaee <hosseinfdnews@gmail.com>
2025-04-20 08:36:46 +0330

    dynamic admin panel

---
+++ panel/partials/footer.php
@@ -0,0 +1,6 @@
+<script src="vendors/bundle.js"></script>
+	<script src="vendors/slick/slick.min.js"></script>
+	<script src="vendors/vmap/jquery.vmap.min.js"></script>
+	<script src="assets/js/app.js"></script>
+</body>
+</html>
