394c249320d66ca6eeb3f0ba3947e12b32555cb2
arashfadaee <hosseinfdnews@gmail.com>
2025-04-20 10:35:39 +0330

    fix bug style login and register page

--- login.php
+++ login.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1">
-    <link href="/assets/" rel="stylesheet" />
-    <link href="/assets/Css/Style.css" rel="stylesheet" />
+    <link href="./assets/Css/Main.css" rel="stylesheet" />
+    <link href="./assets/Css/Style.css" rel="stylesheet" />
 </head>
 <body class="rtl bg-greengrad min-h">
     <section class="container maxw">
@@ -14,18 +14,19 @@
     
                 <div class="col-lg-6">
                     <div class="card-body p-4 text-center">
-                            <a href="#"><img src="/assets/Img/logo-main.png" alt="logo" class="pt-2 pb-4"></a>
+                            <a href="#">
+                                <img src="./assets/Img/logo-main.png" alt="logo" class="pt-2 pb-4"></a>
       
-                        <form action="">
+                        <form action="./panel/action/login.php">
                             <div class="form-group">
-                                <input type="text" name="user" id="user" class="form-control" required placeholder="نام کاربری">
+                                <input type="text" name="mobile" id="user" class="form-control" required placeholder="شماره تلفن ">
                             </div>
                             <div class="form-group mb-4">
                                 <input type="password" name="password" id="password" class="form-control" required placeholder="کلمه عبور">
                             </div>
                             <button type="submit" name="login" id="login" class="btn btn-block py-2 btn-success radius10 my-3">ورود</button>
                     
-                            <p>هنوز ثبت نام نکرده اید ؟ <a href="b_register.html" class="text-drkprimary">عضویت در سایت</a></p>
+                            <p>هنوز ثبت نام نکرده اید ؟ <a href="./register.php" class="text-drkprimary">عضویت در سایت</a></p>
                            </form>
          
                     </div>
--- register.php
+++ register.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1">
-    <link href="/assets/Css/Main.css" rel="stylesheet" />
-    <link href="/assets/Css/Style.css" rel="stylesheet" />
+    <link href="./assets/Css/Main.css" rel="stylesheet" />
+    <link href="./assets/Css/Style.css" rel="stylesheet" />
 </head>
 <body class="rtl bg-greengrad min-h">
     <section class="container maxw">
@@ -14,13 +14,16 @@
            
                 <div class="col-lg-6">
                     <div class="card-body p-4 text-center">
-                            <a href="#"><img src="Img/logo-main.png" alt="logo" class="pt-2 pb-4"></a>
+                            <a href="#"><img src="./assets/Img/logo-main.png" alt="logo" class="pt-2 pb-4"></a>
                         <form action="">
                             <div class="form-group">
                                 <input type="text" name="username" id="username" class="form-control" required placeholder="نام کاربری">
                             </div>
                             <div class="form-group">
                                 <input type="email" name="mail" id="mail" class="form-control" placeholder="آدرس ایمیل">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="mobile" id="mail" class="form-control" placeholder="شماره تلفن همراه">
                             </div>
                             <div class="form-group">
                                 <input type="password" name="password" id="password" class="form-control" required placeholder="کلمه عبور">
@@ -33,7 +36,7 @@
                     </div>
                 </div>
                 <div class="col-lg-6 m-auto">
-                    <img src="Img/login.jpg" class="img-fluid d-lg-block d-none" />
+                    <img src="./assets/Img/login.jpg" class="img-fluid d-lg-block d-none" />
                 </div>
             </div>
         </div>
